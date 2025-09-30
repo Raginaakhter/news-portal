0a98815eaabb20e3f4c22d08759a13c536a1dfd2
Raginaakhter <raginaakhter052@gmail.com>
2025-09-30 23:51:25 +0600

    dded nav-items

--- src/components/Navber.tsx
+++ src/components/Navber.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, Tr } from './ui/navigation-menu'
 
 export const Navber = () => {
     return (
@@ -7,6 +8,27 @@ export const Navber = () => {
 
             <nav className='max-w-7xl mx-auto p-2 sm:p-4 lg:p-3 font-bold text-2xl'>
                 <Link href={"/"}>Daily News</Link>
+
+
+                <div>
+                    <NavigationMenu>
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+
+
+                                <NavigationMenuLink href='/news'>Home</NavigationMenuLink>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger >Services</NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        <NavigationMenuLink href='/services'>Mobile Development</NavigationMenuLink>
+                                        <NavigationMenuLink>Web Development</NavigationMenuLink>
+                                        <NavigationMenuLink>SEO</NavigationMenuLink>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </div>
 
             </nav>
         </header>
